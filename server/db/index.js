acc755581ca7a306c3b52ba9adb4f2e6e93a0b86
Jenny Cho <chojenny112@gmail.com>
2022-04-19 15:25:32 -0400

    model definitions/associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,31 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
-
+const User = require("./models/User");
+const Order = require("./models/Order");
+const Product = require("./models/Product");
+const Review = require("./models/Review");
 
 //associations could go here!
+User.hasMany(Order);
+Order.belongsTo(User);
+
+Product.hasMany(Review);
+Review.belongsTo(Product);
+
+User.hasMany(Review);
+Review.belongsTo(User);
+
+// Product.belongsToMany(Order, { through: "ProductOrder" });
+// Order.belongsToMany(Product, { through: "ProductOrder" });
 
 module.exports = {
   db,
   models: {
     User,
+    Order,
+    Product,
+    Review,
   },
-}
+};
