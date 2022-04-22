12183a14a86a4ca214c81c89fc4118bb27fb7bc0
Olivia Liao <ol64@cornell.edu>
2022-04-22 00:48:38 -0400

    updated the add button to only show when Admin is logged in

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,79 +1,82 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { getFilteredProducts, getProducts } from "../store/products";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getFilteredProducts, getProducts } from '../store/products';
 
 class AllProducts extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.handleSelect = this.handleSelect.bind(this);
-  }
+		this.handleSelect = this.handleSelect.bind(this);
+	}
 
-  componentDidMount() {
-    this.props.loadProducts();
-  }
+	componentDidMount() {
+		this.props.loadProducts();
+	}
 
-  handleSelect(event) {
-    event.target.value === "all-products"
-      ? this.props.loadProducts()
-      : this.props.filterProducts(event.target.value);
-  }
+	handleSelect(event) {
+		event.target.value === 'all-products'
+			? this.props.loadProducts()
+			: this.props.filterProducts(event.target.value);
+	}
 
-  render() {
-    const { products } = this.props;
-    const categories = [
-      "Toner",
-      "Cleanser",
-      "Exfoliator",
-      "Mask",
-      "Moisturizer",
-      "Sun Protection",
-      "Treatments",
-    ];
-    return (
-      <div>
-        <Link to="/products/addForm">
-          <button>Add Product</button>
-        </Link>
-        <div className="product-filter">
-          <h2>All Products</h2>
-          <label className="filter">Filter: </label>
-          <select onChange={this.handleSelect}>
-            <option value="all-products">All Products</option>
-            {categories.map((category) => (
-              <option value={category}>{category}</option>
-            ))}
-          </select>
-        </div>
+	render() {
+		const { products, isAdmin } = this.props;
+		const categories = [
+			'Toner',
+			'Cleanser',
+			'Exfoliator',
+			'Mask',
+			'Moisturizer',
+			'Sun Protection',
+			'Treatments',
+		];
+		return (
+			<div>
+				{isAdmin && (
+					<Link to="/products/addForm">
+						<button>Add Product</button>
+					</Link>
+				)}
+				<div className="product-filter">
+					<h2>All Products</h2>
+					<label className="filter">Filter: </label>
+					<select onChange={this.handleSelect}>
+						<option value="all-products">All Products</option>
+						{categories.map((category) => (
+							<option value={category}>{category}</option>
+						))}
+					</select>
+				</div>
 
-        <div id="products">
-          {products.map((product) => (
-            <div key={product.id} id={product.id} className="product-link">
-              <Link to={`/products/${product.id}`}>
-                <img src={product.imageUrl} />
-                <p>{product.title}</p>
-                <p>{product.price}</p>
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
+				<div id="products">
+					{products.map((product) => (
+						<div key={product.id} id={product.id} className="product-link">
+							<Link to={`/products/${product.id}`}>
+								<img src={product.imageUrl} />
+								<p>{product.title}</p>
+								<p>{product.price}</p>
+							</Link>
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
-const mapState = ({ products }) => {
-  return {
-    products,
-  };
+const mapState = (state) => {
+	return {
+		products: state.products,
+		isAdmin: state.auth.userType === 'Admin',
+	};
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    loadProducts: () => dispatch(getProducts()),
-    filterProducts: (category) => dispatch(getFilteredProducts(category)),
-  };
+	return {
+		loadProducts: () => dispatch(getProducts()),
+		filterProducts: (category) => dispatch(getFilteredProducts(category)),
+	};
 };
 
 export default connect(mapState, mapDispatch)(AllProducts);
--- server/api/users.js
+++ server/api/users.js
@@ -1,61 +1,61 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
-  models: { User, Order },
-} = require("../db");
+	models: { User, Order },
+} = require('../db');
 module.exports = router;
 
-router.get("/", async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ["id", "username"],
-    });
-    res.json(users);
-  } catch (err) {
-    next(err);
-  }
+router.get('/', async (req, res, next) => {
+	try {
+		const users = await User.findAll({
+			// explicitly select only the id and username fields - even though
+			// users' passwords are encrypted, it won't help if we just
+			// send everything to anyone who asks!
+			attributes: ['id', 'username', 'email', 'userType'],
+		});
+		res.json(users);
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      where: {
-        id: req.params.id,
-      },
-      include: [Order],
-      attributes: ["username", "email"],
-    });
-    res.send(user);
-  } catch (err) {
-    next(err);
-  }
+router.get('/:id', async (req, res, next) => {
+	try {
+		const user = await User.findOne({
+			where: {
+				id: req.params.id,
+			},
+			include: [Order],
+			attributes: ['username', 'email'],
+		});
+		res.send(user);
+	} catch (err) {
+		next(err);
+	}
 });
 
 //Admin Privledge to add other admins
-router.post("/admin", async (req, res, next) => {
-  try {
-    const [user, wasCreated] = await User.findOrCreate({
-      where: {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        userType: req.body.userType,
-      },
-    });
-    res.send(user);
-  } catch (err) {
-    next(err);
-  }
+router.post('/admin', async (req, res, next) => {
+	try {
+		const [user, wasCreated] = await User.findOrCreate({
+			where: {
+				username: req.body.username,
+				email: req.body.email,
+				password: req.body.password,
+				userType: req.body.userType,
+			},
+		});
+		res.send(user);
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.delete("/admin/delete/:id", async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req.params.id);
-    await user.destroy();
-    res.send("destroyed user");
-  } catch (err) {
-    next(err);
-  }
+router.delete('/admin/delete/:id', async (req, res, next) => {
+	try {
+		const user = await User.findByPk(req.params.id);
+		await user.destroy();
+		res.send('destroyed user');
+	} catch (err) {
+		next(err);
+	}
 });
