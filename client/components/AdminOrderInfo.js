d1e979c476e9e9c7e619976a226b3f82f53455cc
Wai Yin Tan <waiyinta@gmail.com>
2022-04-26 23:29:01 -0400

    Admin Order List styling

--- client/components/AdminOrderInfo.js
+++ client/components/AdminOrderInfo.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 
 class AdminOrderInfo extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			id: this.props.order.id,
-			status: this.props.order.status,
-			username: this.props.order.user ? this.props.order.user.username : 'guest'
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.order.id,
+      status: this.props.order.status,
+      username: this.props.order.user
+        ? this.props.order.user.username
+        : `Guest: ${this.props.order.email}`,
+    };
+  }
 
-	render() {
-		const { id, username, status } = this.state;
-		return (
-			<tr>
-				<td>{id}</td>
-				<td>{status}</td>
-				<td>{username}</td>
-			</tr>
-		);
-	}
+  render() {
+    const { id, username, status } = this.state;
+
+    return (
+
+          <tr>
+            <td>{id}</td>
+						<td>{status}</td>
+						<td>{username}</td>
+          </tr>
+    );
+  }
 }
+
 
 export default AdminOrderInfo;
